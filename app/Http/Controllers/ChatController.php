faa476ae1e0115462bf7c7b415d561ebd53940ef
Kem Juntilla <kemartus@gmail.com>
2017-11-26 18:24:00 +0800

    update working store function

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -3,8 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
+Use App\Chat;
 
 class ChatController extends Controller
 {
-    //
-}
+
+	public function store(Request $request){
+
+// validate user
+      	Chat::create([
+			'user_id'=>$request->user_id,
+			'message'=>$request->message,
+			]);
+
+		return "ok";
+		}
+	}
