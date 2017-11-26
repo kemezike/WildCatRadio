dafd848e8a2bc43cdc3bf038f239b68e8d0f4044
Kem Juntilla <kemartus@gmail.com>
2017-11-26 18:26:53 +0800

    add chat relationship

--- app/User.php
+++ app/User.php
@@ -26,4 +26,9 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function chat()
+    {
+        return $this->hasMany('App\Chat');
+    }
 }
