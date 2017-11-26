15bb53f4096f345ac17ccb0592914c740be09f6c
Kem Juntilla <kemartus@gmail.com>
2017-11-26 18:35:43 +0800

    remove hard coded time

--- resources/views/app/index.blade.php
+++ resources/views/app/index.blade.php
@@ -2,16 +2,14 @@
 
 
 @section('content')
-
-	<section class="section index" style="">
-		<div class="container">
-			<h1 class="title">
-				1:50 pm
-			</h1>
-			<h2 class="subtitle">
-				Friday, October 3, 2017
-			</h2>
-		</div>
-	</section>
+<section class="section index" style="">
+	<div class="container">
+		<h1 class="title" id="clock-time">
+		</h1>
+		<h2 class="subtitle" id="clock-date">
+		</h2>
+		<div id="txt"></div>
+	</div>
+</section>
 
 @endsection
