5aec0b97527f28347926da84a0ec03869f841a94
Kem Juntilla <kemartus@gmail.com>
2017-11-26 18:28:33 +0800

    add user relation ship

--- app/chat.php
+++ app/chat.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class chat extends Model
 {
-    //
-}
+	protected $fillable = [
+        'user_id','message',
+    ];
+    
+    public function user()
+	{
+		return $this->belongsTo(User::class);
+	}}
