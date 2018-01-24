cf5eb67ef1b6c7ca85b93c95092ec644c504c8c0
Kem Juntilla <kemartus@gmail.com>
2018-01-24 18:26:48 +0800

    reindent

--- public/wcr/js/login.js
+++ public/wcr/js/login.js
@@ -1,13 +1,15 @@
     function register(){
       alert($("#username").val());
-       $.ajax({
-          type: 'post',
-          url: '/register',
-          data: {
-            'name': $("#username").val(),
+
+      $.ajax({
+        type: 'post',
+        url: '/register',
+        data: {
+          'name': $("#username").val(),
         },
         success: function(data) {
+          $("#dedicationTitle").load('/main #dedicationTitle');
          console.log(data)
-     }
- });
-   }
+       }
+     });
+    }
