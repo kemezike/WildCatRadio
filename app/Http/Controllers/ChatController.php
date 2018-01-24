63de9b5eea629b541a7c2305ce6212c961f463f5
Kem Juntilla <kemartus@gmail.com>
2018-01-24 18:20:28 +0800

    change user to auth->user, change query from 5 to 10

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -6,26 +6,26 @@ use Illuminate\Http\Request;
 use App\User;
 Use App\Chat;
 use Carbon\Carbon;
+use Auth;
 
 class ChatController extends Controller
 {
 
 	public function store(Request $request){
-
 // validate user
 		Chat::create([
-			'user_id'=>$request->user_id,
+			'user_id'=>Auth::user()->id,
 			'message'=>$request->message,
 			]);
 
-		return "ok";
+		return Auth::user()->id;
 	}
 
 	public function getChat(){
 		$chats = Chat::leftJoin('users','users.id','=','chats.user_id')
 		->addSelect('name','message','chats.created_at as created_at','chats.updated_at as updated_at')
 		->orderBy('created_at', 'desc')
-		->take(5)->get();
+		->take(10)->get();
 
 		return $chats;
 	}
