03249e715d7d7741911b674a501f8406d82182ca
Kem Juntilla <kemartus@gmail.com>
2018-01-24 18:25:28 +0800

    change recursion variable to 10

--- public/js/displayDedication.js
+++ public/js/displayDedication.js
@@ -11,7 +11,7 @@
         		var h;
         		var m;
                 var s;
-        			for(var i=4;i>=0;i--){
+        			for(var i=9;i>=0;i--){
                             txt += '<div class="container darker"><p>' + data[i].message + '</p><span class="time-left">'+ data[i].name +'</span><br><span class="time-left">College Of Computer Studies</span><br><span class="time-left">' + data[i].created_at + '</span><br><hr></div>'; 
                     }
                     if(txt != ""){
