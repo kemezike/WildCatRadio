8451b0db979b7b00973f28befcfa3c48c7de8f1a
Kem Juntilla <kemartus@gmail.com>
2018-02-14 16:59:36 +0800

    add song name column

--- database/migrations/2017_11_14_085416_create_chats_table.php
+++ database/migrations/2017_11_14_085416_create_chats_table.php
@@ -16,6 +16,7 @@ class CreateChatsTable extends Migration
         Schema::create('chats', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('user_id');
+            $table->string('song_name');
             $table->string('message');
             $table->timestamps();
         });
