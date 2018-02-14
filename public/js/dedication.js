bb60fc8e120a5c9b40ba6c1a4cd4bb38b25ceec9
Kem Juntilla <kemartus@gmail.com>
2018-02-14 17:00:23 +0800

    optimize error modal, add song name in ajax

--- public/js/dedication.js
+++ public/js/dedication.js
@@ -5,12 +5,20 @@
           url: '/chat',
           data: {
             'message': $("#message").val(),
+            'song_name': $("#song-name").val(),
         },
         success: function(data) {
          console.log(data)
          toastr.success('Daghan Salamat!', 'Dedication Sent!');
          $("#decModal").modal('hide');
          $('#message').val("");
+     },
+        error: function(data) {
+         console.log(data)
+
+         toastr.error('Message must not be empty.', 'Oops.');
+         // $("#decModal").modal('hide');
+         // $('#message').val("");
      }
  });
    }
